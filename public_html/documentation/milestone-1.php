77aa07e6dd7589925243b4fca81b892248192429
Victoria Chacon <Vchaco06@gmail.com>
2016-07-20 14:11:17 -0600

    added comments, persona, and use case.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,11 +16,20 @@
 		<br>
 		<!--goal-->
 		<ul>
-			<li>Gain employment, preferrably full time.</li>
+			<li>Gain employment, preferably full time.</li>
 			<li>I would like to be contacted to do freelance work</li>
 			<li>I would like to keep my designs update and reflect that through my site</li>
 			<li>I would like to have a bigger online presence</li>
 		</ul>
-
+		<br>
+		<!--Micheal Vanlandigham's picture-->
+		<img src=images/johndoe.jpg>
+		<!--Persona starts here -->
+		<p><strong>Persona</strong> :Michael Vanlandingham
+		<br>
+		Micheal is the CEO of a graphic design company. He is a Mac user with some experience in PC, but Michael's primary technology would be he Galaxy S5 which he uses constantly. He posts jobs on Indeed and uses Indeed resumes to find potential employees. He is particularly interested in what a designer can do no matter the format in which they work, he requires some sort of portfolio. Online, is more acceptable than on paper, at least with a website/online portfolio he can look through an individuals work quickly and on the go. Michael is busy and cant really sit down and look through a physical portfolio, he is usually busy running around downtown. </p>
+		<br>
+		<!--The Use Case starts here-->
+		<p><strong>Use Case</strong> :It is 6:00pm, Michael has concluded with the meetings for the day. He is scrolling through resumes and decides to check out the websites/online portfolios of the candidates he feels are most qualified. His office is empty now, and he doesn't feel like breaking out his entire computer setup so he wants to use his phone. He is looking for clean design that showcases all of the work at a glance. Too many clicks and he's bound to get annoyed. He wants to have at least three candidates chosen by the end of his night, which he want to be very soon, maybe an hour. He also hopes to find their contact info through the site. He really doesn't want to have to go shuffling through resumes again. </p>
 	</body>
 </html>
