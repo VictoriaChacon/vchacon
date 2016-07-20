2f130acfe4245f136ba8070b7eb8887a9b30732d
Victoria Chacon <Vchaco06@gmail.com>
2016-07-20 11:52:38 -0600

    added purpose and audience and added a list

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,10 +7,20 @@
 	<body>
 		<!--beging header -->
 		<header> Victoria's Personal Website Project</header>
+		<!--begin content-->
+		<!--Purpose-->
 		<p><strong>Purpose</strong>: My personal website will be a portfolio site. What will this accomplish? This will shoe off my work both past and present. I want to use this site in order to promote my skills in the community as well as give employers a look into what I can do for them.</p>
 		<br>
+		<!--Audience-->
 		<p><strong>Audience</strong>: I would like this to be mainy for employers to look at. I am also wanting this to be available to the public who have an interest in design or just as artistic inspiration.</p>
 		<br>
-		<p></p>
+		<!--goal-->
+		<ul>
+			<li>Gain employment, preferrably full time.</li>
+			<li>I would like to be contacted to do freelance work</li>
+			<li>I would like to keep my designs update and reflect that through my site</li>
+			<li>I would like to have a bigger online presence</li>
+		</ul>
+
 	</body>
 </html>
