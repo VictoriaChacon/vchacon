0b7c9dd34ae4585b4017c5c51c41fc4e6bbbdfc4
Victoria Chacon <Vchaco06@gmail.com>
2016-08-04 20:05:25 -0600

    Added html to the milestone-2 project...added the html frame and started writing in a header.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Victoria's Personal Website part 2</title>
+	</head>
+	<body>
+		<!--begin header here-->
+		<header>Victoria's Personal Website Project part 2a</header>
+		<!--begin content here-->
+		
+	</body>
+</html>
