23969669f67d35436461be430677ce47a449b700
Victoria Chacon <Vchaco06@gmail.com>
2016-08-07 15:48:25 -0600

    put in the site map and part of the content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,5 +9,17 @@
 		<header>Victoria's Personal Website Project part 2a</header>
 		<!--begin content here-->
 		<h1>Sitemap:</h1>
+		<ul>
+			<li>Home</li>
+			<li>About Me</li>
+			<li>Portfolio</li>
+			<li>Contact</li>
+		</ul>
+		<br>
+		<h1>Content Strategy</h1>
+		<ul>
+			<li>Home Page: I want this to feature some artwork of mine, im thinking for now a photo. This will then prompt the user to scroll down the page...I want this to be a single scroll page. Im not sure if the website I have in mind has subpages or if it is </li>
+
+		</ul>
 	</body>
 </html>
