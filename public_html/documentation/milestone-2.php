7865807c7a00b01c7b6e46a02e183a5cf5d4d07b
Victoria Chacon <Vchaco06@gmail.com>
2016-08-07 19:07:51 -0600

    Finished replacing the content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,7 +18,10 @@
 		<br>
 		<h1>Content Strategy</h1>
 		<ul>
-			<li>Home Page: I want this to feature some artwork of mine, im thinking for now a photo. This will then prompt the user to scroll down the page...I want this to be a single scroll page. Im not sure if the website I have in mind has subpages or if it is </li>
+			<li>Home Page: I want this to feature some artwork of mine, im thinking for now a photo. This will then prompt the user to scroll down the page...I want this to be a single scroll page. </li>
+			<li>About Me Page: I want this page to summarize my accomplishments and work. This would be my artist statement. I would let users know my style and what my thought processes are like when I work. </li>
+			<li>Portfolio: I want this to be an image gallery on the left, and when you click on an image I would like it to show a description on the right stating the name of the project and a what I used to create the projects, back-story of the project, etc. I will probably only have about 4 pieces of work here for now. This will expand as my web/dsign career expands.</li>
+			<li>Contact Page:This page will forward to my gmail through the webpage. I will have my contact through email, as my workload increases I will have a Facebook for my brand. For now I will limit it to email.</li>
 
 		</ul>
 	</body>
